f93bb8d9c9f6d55d5b679c1a171fcacf898dbe01
pdub187 <41226541+pdub187@users.noreply.github.com>
2018-07-20 18:34:14 +1000

    Add files via upload

---
+++ collatzing my brain.py
@@ -0,0 +1,14 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    else:
+        print(3 * number + 1)
+        return 3 * number + 1
+print('Please type a number.')
+number = int(input())
+while number !=1:
+    number = collatz(number)
+ 
+
+             
